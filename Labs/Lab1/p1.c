0ad22d1bc7c93d73ef145fdf6e05cbf4a4a5bb94
pgarcriu <100428976@alumnos.uc3m.es>
2024-02-07 19:16:31 +0100

    p1 hecho

--- Labs/Lab1/p1.c
+++ Labs/Lab1/p1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(int argo, char **argv)
+int main(int argc, char **argv)
 {
-	printf("Programa P1 \n");
-
+	if(argc > 1){
+		for(int i = 1;i < argc; i++){
+			printf("Argumento %d = %s\n",i,argv[i]);
+		}
+	}else{
+		printf("No se pasaron argumentos\n");
+	}
+	
 	return 0;
 }
