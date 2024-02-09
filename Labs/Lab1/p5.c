f1b51265f3af24cf38e13cc1edcc63d71ddc7977
pgarcriu <100428976@alumnos.uc3m.es>
2024-02-09 09:49:39 +0100

    p5 terminado

--- Labs/Lab1/p5.c
+++ Labs/Lab1/p5.c
@@ -2,38 +2,37 @@
 #include <stdlib.h>
 #include "p5LinkedList.h"
 
-void readArgs(int argc, char **argv, char **array){
+void readArgs(int argc, char **argv, struct LinkedList *list){
     // Ya que las cadenas de caracteres tienen un tamaño indeterminado, 
     // el array contendrá punteros a estas cadenas de caracteres
 	for(int i = 1;i < argc; i++){
         char *current_string = argv[i];
-        array[i - 1] = current_string;
+        char* end;
+			long int value = strtol(argv[i],&end,10);
+			if(*end == '\0'){
+				// Valor transformado a entero
+                push(list, current_string, value);
+			}else{
+                push(list, current_string, 0);
+            }
+        
 		}
         
 }
 
-void printArray(int length, char **array){
-    printf("[");
-    for (int i = 0; i < length; i++){
-            printf("%s",array[i]);
-            if(i < length - 1){
-                printf(", ");
-            }
-        }
-    printf("]\n");
+void printArray(struct LinkedList *list){
+    print(list);
 }
 
 int main(int argc, char **argv)
 {
 	if(argc > 1){
-        // Definimos el tamaño del array previamente contando el número de parámetros que introduce el usuario
-        int array_length = argc - 1;
         // Inicialización de la lista enlazada
         struct LinkedList * list;
         list = init();
 
-		// readArgs(argc, argv, array);
-        // printArray(array_length, array);
+		readArgs(argc, argv, list);
+        printArray(list);
 
 	}else{
 		printf("No se pasaron argumentos\n");
